3fa250a5cd88c721720105dc4b74cd7c549d11ba
ws2vicky <ws2vicky@gmail.com>
2022-08-19 20:51:49 +0800

    json方法补充

--- JSON.stringify.js
+++ JSON.stringify.js
@@ -1,57 +1,59 @@
-// let defaultData = [
-// 	{
-// 		name: "活动名称一",
-// 		description: "这个活动真好玩",
-// 		type: "string",
-// 		required: "false",
-// 		isShown: "false",
-// 	},
-// 	{
-// 		name: "活动名称二",
-// 		description: "这个活动真好玩",
-// 		type: "number",
-// 		required: "false",
-// 		isShown: "true",
-// 		children: [
-// 			{
-// 				name: "活动名称123",
-// 				description: "这个活动真好玩",
-// 				type: "integer",
-// 				required: "false",
-// 				isShown: "false",
-// 				children: [
-// 					{
-// 						name: "活动名称456",
-// 						description: "这个活动真好玩",
-// 						type: "boolean",
-// 						required: "false",
-// 						isShown: "false",
-// 						children: [
-// 							{
-// 								name: "活动名称789",
-// 								description: "这个活动真好玩",
-// 								type: "object",
-// 								required: "false",
-// 							},
-// 						],
-// 					},
-// 				],
-// 			},
-// 		],
-// 	},
-// ];
+// 数组
+let defaultData = [
+	{
+		name: "活动名称一",
+		description: "这个活动真好玩",
+		type: "string",
+		required: "false",
+		isShown: "false",
+	},
+	{
+		name: "活动名称二",
+		description: "这个活动真好玩",
+		type: "number",
+		required: "false",
+		isShown: "true",
+		children: [
+			{
+				name: "活动名称123",
+				description: "这个活动真好玩",
+				type: "integer",
+				required: "false",
+				isShown: "false",
+				children: [
+					{
+						name: "活动名称456",
+						description: "这个活动真好玩",
+						type: "boolean",
+						required: "false",
+						isShown: "false",
+						children: [
+							{
+								name: "活动名称789",
+								description: "这个活动真好玩",
+								type: "object",
+								required: "false",
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+];
+// 修改数组 key
+const a = JSON.stringify(
+	defaultData,
+	(key, value) =>
+		(value =
+			value.children != null
+				? ((value.evil = value.children), delete value.children, value)
+				: value)
+);
 
-// const a = JSON.stringify(
-// 	defaultData,
-// 	(key, value) =>
-// 		(value =
-// 			value.children != null
-// 				? ((value.evil = value.children), delete value.children, value)
-// 				: value)
-// );
+console.log(JSON.parse(a));
 
-// console.log(JSON.parse(a));
-
+// 对象
 const obj = {
 	name: "why",
 	age: 18,
@@ -64,6 +66,16 @@ const obj = {
 	// }
 };
 
-source = JSON.stringify(obj);
-var b = source.replace(new RegExp("name", "g"), "id");
-console.log(b);
+// 修改key
+// source = JSON.stringify(obj);
+// var b = source.replace(new RegExp("name", "g"), "id");
+// console.log(b);
+
+// 删除不需要的key   name
+var JsonObj = JSON.stringify(obj, (key, value) => {
+	console.log("value", value);
+	return key == "name" ? undefined : value;
+});
+// key == "name" ? undefined : (0, value)
+
+console.log(JsonObj);
--- 获取对象深层属性值.js
+++ 获取对象深层属性值.js
@@ -11,4 +11,7 @@ const data = {
 
 const fn1 = (data, attr) => eval(`data['${attr.split("/").join(`']['`)}']`)
 
+// const fn = (data, attr) =>:
+
+console.log("a/b/c".split("/").reduce((pre, cur) => pre[cur], data))
 console.log(fn1(data, "a/b/c"))
